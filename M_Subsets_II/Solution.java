7adb660dd65bd422df2ca212645a563d0089a625
Jeremy Wei <jeremy.t.wei@gmail.com>
2017-11-23 21:05:08 -0500

    finished subsets II

---
+++ M_Subsets_II/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Solution {
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    Arrays.sort(nums);
+    List<List<Integer>> answers = new ArrayList<>();
+    backtrack(answers, new ArrayList<>(), nums, 0);
+    return answers;
+  }
+
+  public void backtrack (List<List<Integer>> answers, List<Integer> tempList, int[] nums, int start) {
+    answers.add(new ArrayList<>(tempList));
+    for (int i = start; i < nums.length; i++) {
+      if (i > start && nums[i] == nums[i-1]) continue;
+      tempList.add(nums[i]);
+      backtrack (answers, tempList, nums, i+1);
+      tempList.remove(tempList.size() - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution S = new Solution();
+    int[] nums1 = {1,2,2};
+    System.out.println(S.subsetsWithDup(nums1));
+
+  }
+}
