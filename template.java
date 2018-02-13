a532fa73b8b3443000472b806184a069e7fd319f
Jeremy Wei <jeremy.t.wei@gmail.com>
2018-02-13 15:25:51 -0500

    updated template

--- template.java
+++ template.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class Solution {
 
-  public static void main(String[] args) {
-    Solution S = new Solution();
+    public static void main(String[] args) {
+        Solution S = new Solution();
 
-  }
+    }
 }
